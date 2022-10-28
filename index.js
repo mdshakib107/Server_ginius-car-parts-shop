ae73cea117c33012c00ac8b185ca2a55f436c58e
mdshakib107 <mdshakib105100@gmail.com>
2022-10-28 09:11:28 +0600

    66 module Done

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ async function run() {
             res.send(result);
         });
 
+        // DELETE
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
     } finally {
         // await client.close();
     }
